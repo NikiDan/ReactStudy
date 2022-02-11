57ce06349170222c96196d2295fac130e7e5a1c2
Nikita <novakhudonosor@mail.ru>
2022-02-11 17:01:01 +0300

    Update 2.1
    Animations

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import './Animation.css';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './Animation.css';
+import 'animate.css';
+
 
 function App(){
 
@@ -20,40 +21,47 @@ function App(){
         }
         else {
         }
+        document.getElementById('textInput').value = "";
     }
     const getElement = (e) => {
          setNewValue(e.target.value)
     }
     const handleRemove = (deleteIndex) =>{
-        console.log(deleteIndex)
+        const list  = document.getElementById('mainList')
+
+        list.addEventListener('animationend', () => {
+
+             list.classList.add('animate__bounceOutLeft')
+        } )
+
         setValue(value.filter((value, index)=> index !== deleteIndex));
+
     }
 
     return(
+
         <div className="App">
             <header>
                 <form className="formToDo" onSubmit={(event => {event.preventDefault()})}>
                     <h1>What's you need to do today ?</h1>
                     <input type="text"
                            placeholder="Enter your text"
+                           id="textInput"
                            onChange={getElement}
                     >
                     </input>
                     <Button variant = "outlined" onClick={Add} type="submit">Add</Button>
                 </form>
             </header>
-            <TransitionGroup component="div">
                 {value.map((value, index)=>
-                        <CSSTransition key={value} timeout={200} classNames="animation">
-                            <div className="mainList" key={index}>
-                                <span className="listItem">{value}</span>
-                                <IconButton onClick={()=>handleRemove(index)}>
-                                    <DeleteIcon/>
-                                </IconButton>
-                            </div>
-                        </CSSTransition>
+                        <div className="mainList animate__animated animate__flipInX" key={index} id="mainList">
+                            <span className="listItem">{value}</span>
+                            <IconButton onClick={()=>handleRemove(index)}>
+                                <DeleteIcon/>
+                            </IconButton>
+                        </div>
                     )
-                }</TransitionGroup>
+                }
         </div>
     );
 }
